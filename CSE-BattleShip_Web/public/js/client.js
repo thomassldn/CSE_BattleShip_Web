d72fe5697772d6fd54d9d68261b7744eb57097be
AlexisN2196 <79947138+AlexisN2196@users.noreply.github.com>
2021-05-06 16:13:07 -0700

    Add files via upload

--- CSE-BattleShip_Web/public/js/client.js
+++ CSE-BattleShip_Web/public/js/client.js
@@ -1,11 +1,14 @@
 
 let locators = document.querySelectorAll('.locator');
+let computerBoard = document.getElementById('computerboard');
+//let userBoard = document.getElementById('userboard');
 if(locators !== null){
     for(const locator of locators){
         locator.addEventListener('click', function(e){
             e.preventDefault(); //prevent regular form behavior
             //let coords = e.target.value.replace(/\s/, '');
             let coords = e.target.dataset.id;
+            let computerCoords = Number(coords) + 100;
             console.log("Coordinate send to back-end: ", coords);
             //send to firebase and handle response
             fetch('/gameboard', {
@@ -21,10 +24,18 @@ if(locators !== null){
                 if(hit){
                     locator.classList.add('hit');
                     locator.value = 'X'
+                    for(const locator of computerBoard.querySelectorAll('.locator')){
+                        if(Number(locator.dataset.id) == computerCoords) {
+                            locator.classList.add('hit'); 
+                            locator.value = 'X';
+                        }
+                    }
+                    //console.log(computerBoard[index])
                 } else {
 
                     locator.classList.add('miss');
                     locator.value = 'miss'
+                    
                     
                 }
             });
