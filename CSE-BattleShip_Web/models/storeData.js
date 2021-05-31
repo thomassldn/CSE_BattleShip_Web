7cc31841203c6244a179a1d22cf11b71e6229690
thomassldn <003686838@coyote.csusb.edu>
2021-05-31 11:59:05 -0700

    Add files via upload

--- CSE-BattleShip_Web/models/storeData.js
+++ CSE-BattleShip_Web/models/storeData.js
@@ -4,29 +4,46 @@ const admin = require('firebase-admin');
 //Key that allows our application to talk to FireStore - Keep this safe
 const serviceAccount = require('../config/cse17b-battleship-firebase-adminsdk-o6lta-1f026ed645.json');
 var coordinate = "00";
+var dataObj =  {
+    Player1:{coordinate:"000"},
+    Player2:{coordinate:"00"}
+  };
+
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
 
-let docRef = db.collection('BattleShip').doc('Player1');
+//let docRef = db.collection('BattleShip').doc('Player1');
 
 //FUNCTION sendDataToFirestore()
 var sendDataToFirestore = function (coordinate, game_room){
 
-  console.log("COOOOOOR1:", coordinate, " ", game_room.game_id);
 
-  //Create an object to hold clients information
-  var dataObj = {coordinate: coordinate};
-  /*
-  dataObj[user] = {
-    coordinate: coordinate
-  };
-  */
+
+  console.log("GAMEEEEE:", game_room.slice(0,-2));
+
+  if( game_room.slice(-1) == 2){ //determine if player 1 is storing this coordinate or player 2
+    console.log("This is player 2");
+    game_room = game_room.slice(0,-2);
+    //Create an object to hold clients information
+    dataObj.Player2.coordinate = coordinate;
+
+  } else { //Player 1
+    console.log("This is player 1");
+    //Create an object to hold clients information
+    dataObj.Player1.coordinate = coordinate;
+
+
+  }
+  console.log("GameROOOOM: ", game_room);
+
+
+
    // Add a new map field in collection "BattleShip", document "coordinates"
-
-   let docRef = db.collection('BattleShip').doc('GamesRoom_' + game_room.game_id).set({ Player1:{coordiante:"00"}, Player2:{coordinate:"01"} });
+   let docRef = db.collection('BattleShip').doc('GamesRoom_' + game_room);//{ Player1:{coordiante:"09"}, Player2:{coordinate:"01"} }
    let checkInClient = docRef.set(dataObj, {merge: true})
    .then(function() {
        console.log("4) Coordinate stored in Firestore database!");
